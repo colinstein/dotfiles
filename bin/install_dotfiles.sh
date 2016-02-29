72a1766134c7a181acc0d2831768bc9c18673d45
Colin Stein <colinstein@me.com>
2016-02-29 07:14:08 -0500

    starting to symlink things

--- bin/install_dotfiles.sh
+++ bin/install_dotfiles.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-printf "Symlink all the things…"
+DOTFILES_BASE="`dirname \"$0'"`"
+DOTFILES_BASE="`(cd \"$DOTFILES_BASE\"/.. && pwd)`"
+
+printf "Zsh: "
+ln -sF "$DOTFILES_BASE/zlogin" ~/.zlogin
+ln -sF "$DOTFILES_BASE/zlogout" ~/.zlogout
+ln -sF "$DOTFILES_BASE/zshenv" ~/.zshenv
+ln -sF "$DOTFILES_BASE/zprofile" ~/.zprofile
+ln -sF "$DOTFILES_BASE/zshrc" ~/.zshrc
+printf "done.\n"
+
